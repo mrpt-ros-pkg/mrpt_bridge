2a3e7397cc4f3189735fb560c289e4709618fe8d
Max Kuzmin <max_kuzmin@outlook.com>
2018-09-28 12:01:01 -0700

    Fixed build errors

--- include/mrpt_bridge/time.h
+++ include/mrpt_bridge/time.h
@@ -3,6 +3,7 @@
 
 #include <mrpt/system/datetime.h>
 #include <ros/time.h>
+#include <mrpt/version.h>
 
 namespace mrpt_bridge
 {
@@ -15,7 +16,11 @@ inline void convert(const ros::Time& src, mrpt::system::TTimeStamp& des)
 {
 	// return (((uint64_t)src.sec) * (uint64_t)10000000) +
 	// ((uint64_t)116444736*1000000000);
+#if MRPT_VERSION>=0x199
+	des = mrpt::system::time_tToTimestamp((double)src.sec + ((double)src.nsec / 1000000000.d));
+#else
 	des = mrpt::system::time_tToTimestamp((time_t)src.sec) + src.nsec / 100;
+#endif
 	// printf("nsec    %" PRIu64 "\n", des);
 }
 
@@ -26,9 +31,14 @@ inline void convert(const ros::Time& src, mrpt::system::TTimeStamp& des)
   */
 inline void convert(const mrpt::system::TTimeStamp& src, ros::Time& des)
 {
+#if MRPT_VERSION>=0x199
+	des.sec = (uint64_t)mrpt::system::timestampTotime_t(src);
+	des.nsec = (uint64_t)(mrpt::system::timestampTotime_t(src) * 1000000000);
+#else
 	des.sec = ((uint64_t)src) / ((uint64_t)10000000) -
 			  (((uint64_t)116444736) * ((uint64_t)100));
 	des.nsec = ((uint64_t)src) % ((uint64_t)10000000) * 100;
+#endif
 	// printf("ros.sec       %" PRIu32 "\n", des.sec);
 	// printf("ros.nsec      %" PRIu32 "\n", des.nsec);
 }
