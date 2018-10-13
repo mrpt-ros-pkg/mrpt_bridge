4b6323ca144c213df7949ec0631d9d4806ec4658
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-10-13 23:24:25 +0200

    fix build against mrpt2
    Fixed #5

--- include/mrpt_bridge/time.h
+++ include/mrpt_bridge/time.h
@@ -7,31 +7,18 @@
 namespace mrpt_bridge
 {
 /**
-  * converts ros time to mrpt time
-  * @param src ros time
-  * @param des mrpt time
-  */
-inline void convert(const ros::Time& src, mrpt::system::TTimeStamp& des)
-{
-	// return (((uint64_t)src.sec) * (uint64_t)10000000) +
-	// ((uint64_t)116444736*1000000000);
-	des = mrpt::system::time_tToTimestamp((time_t)src.sec) + src.nsec / 100;
-	// printf("nsec    %" PRIu64 "\n", des);
-}
+ * converts ros time to mrpt time
+ * @param src ros time
+ * @param des mrpt time
+ */
+void convert(const ros::Time& src, mrpt::system::TTimeStamp& des);
 
 /**
-  * converts mrpt time to ros time
-  * @param src ros time
-  * @param des mrpt time
-  */
-inline void convert(const mrpt::system::TTimeStamp& src, ros::Time& des)
-{
-	des.sec = ((uint64_t)src) / ((uint64_t)10000000) -
-			  (((uint64_t)116444736) * ((uint64_t)100));
-	des.nsec = ((uint64_t)src) % ((uint64_t)10000000) * 100;
-	// printf("ros.sec       %" PRIu32 "\n", des.sec);
-	// printf("ros.nsec      %" PRIu32 "\n", des.nsec);
-}
+ * converts mrpt time to ros time
+ * @param src ros time
+ * @param des mrpt time
+ */
+void convert(const mrpt::system::TTimeStamp& src, ros::Time& des);
 
 };  // namespace mrpt_bridge
 
